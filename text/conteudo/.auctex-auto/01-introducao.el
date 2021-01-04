0c873198c2cf7ff941b019f1fd936c583b67616f
C. Lente <7017340+clente@users.noreply.github.com>
2021-01-04 00:19:58 -0300

    Hypothesis

--- text/conteudo/.auctex-auto/01-introducao.el
+++ text/conteudo/.auctex-auto/01-introducao.el
@@ -6,26 +6,6 @@
     "sec:social_networks"
     "sec:recommender_systems"
     "sec:radicalization"
-    "sec:hypothesis"
-    "sec:consideracoes_preliminares")
-   (LaTeX-add-index-entries
-    "Notas de rodapé"
-    "natbib"
-    "Língua estrangeira"
-    "Legendas"
-    "Floats"
-    "bibtex"
-    "biblatex"
-    "biber"
-    "Google Scholar"
-    "Web of Science"
-    "Scopus"
-    "Zotero"
-    "Mendeley"
-    "CiteULike"
-    "Tese/Dissertação!versões"
-    "Formatação"
-    "Tese/Dissertação!itens obrigatórios"
-    "Tese/Dissertação!itens opcionais"))
+    "sec:hypothesis"))
  :latex)
 
