eeb6a053c06c9632d4c177fdcdf9a8d60edc3dfa
C. Lente <7017340+clente@users.noreply.github.com>
2021-04-26 10:24:15 -0300

    TFRS

---
+++ code/06_tf_recommenders.R
@@ -0,0 +1,101 @@
+
+# # Setup env
+# reticulate::miniconda_update()
+# reticulate::conda_create("tfrs", python_version = "3.8")
+
+# # Install dependencies
+# reticulate::conda_install("tfrs", "tensorflow==2.4.0", pip = TRUE)
+# reticulate::conda_install("tfrs", "tensorflow_recommenders", pip = TRUE)
+# reticulate::conda_install("tfrs", "tensorflow_datasets", pip = TRUE)
+
+# Load R packages
+library(reticulate)
+
+# Load env
+use_condaenv("tfrs", required = TRUE)
+
+# Import typing
+typing <- import("typing", convert = FALSE)
+Dict   <- typing$Dict
+Text   <- typing$Text
+
+# Modules
+np   <- import("numpy", convert = FALSE)
+tf   <- import("tensorflow", convert = FALSE)
+tfds <- import("tensorflow_datasets", convert = FALSE)
+tfrs <- import("tensorflow_recommenders", convert = FALSE)
+
+# Datasets
+ratings <- tfds$load("movielens/100k-ratings", split = "train")
+movies  <- tfds$load("movielens/100k-movies", split = "train")
+
+# Select the basic features
+ratings <- ratings$map(function(x) { list(movie_title = x[["movie_title"]], user_id = x[["user_id"]]) })
+movies  <- movies$map(function(x) { x[["movie_title"]] })
+
+user_ids_vocabulary <- tf$keras$layers$experimental$preprocessing$StringLookup(mask_token = NULL)
+user_ids_vocabulary$adapt(ratings$map(function(x) { x[["user_id"]] }))
+
+movie_titles_vocabulary <- tf$keras$layers$experimental$preprocessing$StringLookup(mask_token = NULL)
+movie_titles_vocabulary$adapt(movies)
+
+MovieLensModel <- PyClass("MovieLensModel", list(
+  
+  `__init__` = function(self,
+                        user_model = tf$keras$Model,
+                        movie_model = tf$keras$Model,
+                        task = tfrs$tasks$Retrieval) {
+    
+    super()$`__init__`()
+    
+    # Set up user and movie representations
+    self$user_model <- user_model
+    self$movie_model <- movie_model
+    
+    # Set up a retrieval task
+    self$task <- task
+    
+    return(NULL)
+  },
+  
+  compute_loss = function(self,
+                          features = list(),
+                          training = FALSE) {
+    
+    # Define how the loss is computed.
+    user_embeddings <- self$user_model(features[["user_id"]])
+    movie_embeddings <- self$movie_model(features[["movie_title"]])
+    return(self$task(user_embeddings, movie_embeddings))
+  }
+), inherit = tfrs$Model)
+
+# Define user and movie models
+user_model <- tf$keras$Sequential(c(
+  user_ids_vocabulary,
+  tf$keras$layers$Embedding(user_ids_vocabulary$vocab_size(), 64L)
+))
+movie_model <-tf$keras$Sequential(c(
+  movie_titles_vocabulary,
+  tf$keras$layers$Embedding(movie_titles_vocabulary$vocab_size(), 64L)
+))
+
+# Define your objectives
+task <- tfrs$tasks$Retrieval(metrics = tfrs$metrics$FactorizedTopK(
+  movies$batch(128L)$map(movie_model)
+))
+
+# Create a retrieval model
+model <- MovieLensModel(user_model, movie_model, task)
+model$compile(optimizer = tf$keras$optimizers$Adagrad(0.5))
+
+# Train for 3 epochs
+model$fit(ratings$batch(4096L), epochs = 3L)
+
+# Use brute-force search to set up retrieval using the trained representations
+index <- tfrs$layers$factorized_top_k$BruteForce(model$user_model)
+index <- index$index(movies$batch(100L)$map(model$movie_model), movies)
+
+# Get some recommendations
+titles <- py_to_r(index$call(np$array(list("42")))[[1]][0]$numpy()$tolist())
+titles <- purrr::map_chr(titles, ~.x$decode("utf-8"))
+print(paste0("Top 3 recommendations for user 42: ", paste0(titles, collapse = ", ")))
